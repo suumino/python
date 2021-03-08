eed38fde9c84a0a6236fe479daa4ed5ac8a54d6a
suumino <78770211+suumino@users.noreply.github.com>
2021-03-08 17:18:47 +0900

    Add files via upload

--- hello_flask2.py
+++ hello_flask2.py
@@ -1,19 +1,18 @@
 
-from flask import Flask, render_template
-from vsearch2 import search4letters
+from flask import Flask
+from vsearch import search4letters
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello() -> str:
     return 'Hello world from Flask!'
 
-@app.route('/search4')
-def do_search() ->str:
-    return str(search4letters('life, the universe and everyting', 'eiru,!'))
 
-@app.route('/entry')
-def entry_page() -> 'html':
-    return render_template('entry.html', 
-                           the_title='Welcome to search4letters on the web!')
+@app.route('/search4')
+def do_search() -> str:
+    return str(search4letters('life, the universe, and everything', 'eiru,!'))
+
 
 app.run()
